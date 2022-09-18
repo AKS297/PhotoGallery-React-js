876f7f1e6ed8eaaa96b9de0b5a6cbcad353e6cac
Akshay <akshayimmortal297@gmail.com>
2022-09-18 12:51:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Images from './Images';
+import { useState } from 'react';
 
 function App() {
+  const[selectedImage,setSelectedImage]=useState (Images[0])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='container'>
+     <img src={selectedImage} alt='selectedImage' className='selected' />
+     <div className='imageHolder'>
+      {
+        Images.map((img,index)=>(
+          <img 
+          style={{border:selectedImage ===img ? "4px solid purple" :""}}
+          key={index} src={img} alt='image'
+          onClick={()=> setSelectedImage(img)}
+           />
+         
+        ))
+      }
+     </div>
+
+     </div>
     </div>
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,13 @@
+const Images=[
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMtAgslBqFAhrydwVznYgiMUNxbnAKcCuiLW1TMil-&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVo7zYmqXLs6t9gO3A938RbP3JQBeAkrU_xePnsmAOj7xa9dfgRx_JFD0PKJ67MLSieds&usqp=CAU",
+    "https://m.media-amazon.com/images/I/71CNQ8Njn+L._SL1500_.jpg",
+    "https://i.pinimg.com/736x/66/54/1c/66541c2f54167b806edcec2f2405b678.jpg",
+    "https://i.pinimg.com/originals/70/e8/95/70e8957ec64793bcf30f0666c7c0b96e.jpg",
+    "https://i.pinimg.com/736x/58/7f/d8/587fd87130aa495ea2eb74c51e94708b.jpg",
+    "https://i.pinimg.com/originals/18/6f/43/186f431258901b1bba8461563acda329.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNicqx6XQnTrN8TBMY2RBfyMVCN3WsW_M0dg&usqp=CAU",
+    "https://wallpaper.dog/large/20370517.jpg",
+    "https://c4.wallpaperflare.com/wallpaper/970/4/1013/flowers-swan-waterfall-wallpaper-preview.jpg"
+]
+export default Images;
